3f1ec994aea49b6c2b3bf0e98dac213cc2511f96
baburam-nabik <baburam_nabik@bpointer.in>
2026-05-16 16:19:47 +0530

    test 24

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3781773218" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1752149724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
